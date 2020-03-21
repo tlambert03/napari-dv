b82cb0ac5f6fb9e42e7ab6e4a533867f9055b54f
Talley Lambert <talley.lambert@gmail.com>
2020-03-21 18:48:18 -0400

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="napari-dv",
-    version="0.2.2",
+    version="0.2.3",
     author="Talley Lambert",
     author_email="talley.lambert@gmail.com",
     maintainer="Talley Lambert",
